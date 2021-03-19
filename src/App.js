640d412478131e5076bd514841b01a29136dd153
asadsanto10 <santo.meridian@gmail.com>
2021-03-19 22:19:04 +0600

    react form handel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import ClockList from './component/ClockList';
+// import ClockList from './component/ClockList';
+import From from './component/From';
 
 function App() {
-  const quantities = [1, 2, 3];
+  // const quantities = [1, 2, 3];
   return (
     <div className="App">
-      <ClockList quantities={quantities} />
+      {/* <ClockList quantities={quantities} /> */}
+      <From />
     </div>
   );
 }
