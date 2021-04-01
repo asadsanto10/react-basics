942ba6f001e63f864420d30b794188c43fab27f3
asadsanto10 <santo.meridian@gmail.com>
2021-04-01 10:46:46 +0600

    Composition vs Inheritance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // import ClockList from './component/ClockList';
-import Calculator from './component/Calculator';
 
 function App() {
   // const quantities = [1, 2, 3];
   return (
     <div className="App">
-      <Calculator />
+      <h1>lof</h1>
     </div>
   );
 }
