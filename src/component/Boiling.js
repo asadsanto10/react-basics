13f5d0309bb3e61ab5e372068456e8ba0462b037
asadsanto10 <santo.meridian@gmail.com>
2021-04-14 00:14:35 +0600

    higher-order-component

--- src/component/Boiling.js
+++
@@ -1,9 +0,0 @@
-const Boiling = ({ celsius = 0 }) => {
-  if (celsius >= 100) {
-    return <p>The water would boil</p>;
-  }
-
-  return <p>The water would not boil</p>;
-};
-
-export default Boiling;
