2ec86cca5133aa1f444b98d4ccea8765b2eabda9
asadsanto10 <santo.meridian@gmail.com>
2021-06-13 13:59:00 +0600

    useref

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import Button from './component/Button';
-import ShowCount from './component/ShowCount';
-import Title from './component/Title';
-
-// useMemo and useHook performance optimze hisebe kaj kore
+import React from 'react';
+import Form from './component/Form';
 
 const App = () => {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-
-  const incrementByOne = useCallback(() => {
-    setCount1((prev) => prev + 1);
-  }, []);
-
-  const incrementByFive = useCallback(() => {
-    setCount2((prev) => prev + 5);
-  }, []);
-
-  const evenOdd = useMemo(() => {
-    let i = 0;
-    while (i < 1000) i += 1;
-    return count1 % 2 === 0;
-  }, [count1]);
   return (
     <div>
-      <Title />
-      <ShowCount count={count1} title="Couner one" />
-      <span>{evenOdd ? 'Even' : 'Odd'}</span>
-      <Button handelClick={incrementByOne}>increment By One</Button>
-      <hr />
-      <ShowCount count={count2} title="Couner five" />
-      <Button handelClick={incrementByFive}>increment By five</Button>
+      <Form />
     </div>
   );
 };
---
+++ src/component/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter Something" />
+    </div>
+  );
+};
+
+export default Form;
