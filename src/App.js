7ec988f5708f9930f15f60d331a2ffb5c96da250
asadsanto10 <santo.meridian@gmail.com>
2021-06-27 13:18:13 +0600

    component-style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Layout from './component/Layout';
-import LayoutTwo from './component/LayoutTow';
+import './assets/css/global.css';
+import Logo from './component/Logo';
 
-const REACT_VERSION = React.version;
 const App = () => {
   return (
     <div>
-      <h1>{REACT_VERSION}</h1>
-      <Layout />
-      <LayoutTwo />
+      <Logo />
     </div>
   );
 };
